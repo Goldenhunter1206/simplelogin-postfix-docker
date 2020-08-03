83e1296e8df49549fa6c6b8a4c250578a2042a21
Goldenhunter <moritz@stoesslein.com>
2020-08-03 19:55:20 +0200

    removed certbot

--- generate_config.py
+++ generate_config.py
@@ -13,15 +13,6 @@ from pathlib import Path
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 from jinja2.exceptions import UndefinedError
 
-# Certbot
-CERTBOT_CONFIG_DIR = Path('/etc/letsencrypt')
-CERTBOT_CONFIG_FILENAME = 'cli.ini'
-
-# Let's Encrypt
-LETSENCRYPT_CONFIG_DIR = CERTBOT_CONFIG_DIR / 'live'
-LETSENCRYPT_CERTIFICATE = 'fullchain.pem'
-LETSENCRYPT_PRIVATE_KEY = 'privkey.pem'
-
 # Postfix
 POSTFIX_CONFIG_DIR = Path('/etc/postfix')
 POSTFIX_CONFIG_FILENAMES = ['main.cf', 'pgsql-relay-domains.cf', 'pgsql-transport-maps.cf']  # noqa: E501
@@ -36,13 +27,6 @@ templates = Environment(
 )
 
 
-def generate_certbot_config():
-    """Generate Certbot's configuration file."""
-    with (CERTBOT_CONFIG_DIR / CERTBOT_CONFIG_FILENAME).open('w') as f:
-        template = templates.get_template(f'certbot/{CERTBOT_CONFIG_FILENAME}')
-        f.write(template.render(env=environ))
-
-
 def generate_postfix_config():
     """Generate Postfix's configuration files."""
     for config in POSTFIX_CONFIG_FILENAMES:
@@ -51,26 +35,16 @@ def generate_postfix_config():
 
             # Check if Certbot generated a TLS certificate.
             postfix_fqdn = environ['POSTFIX_FQDN']
-            cert_file = LETSENCRYPT_CONFIG_DIR / postfix_fqdn / LETSENCRYPT_CERTIFICATE
-            key_file = LETSENCRYPT_CONFIG_DIR / postfix_fqdn / LETSENCRYPT_PRIVATE_KEY
-
-            enable_tls = cert_file.is_file() and key_file.is_file()
 
             # Generate config file.
             f.write(template.render(
                 env=environ,
-                tls=enable_tls,
-                tls_cert=cert_file,
-                tls_key=key_file,
             ))
 
 
 def main():
     """Generate Certbot and/or Postfix's configuration files."""
     try:
-        if '--certbot' in sys.argv or len(sys.argv) == 1:
-            generate_certbot_config()
-
         if '--postfix' in sys.argv or len(sys.argv) == 1:
             generate_postfix_config()
 
--- scripts/certbot-renew-posthook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-/src/generate_config.py --postfix
-postfix reload
